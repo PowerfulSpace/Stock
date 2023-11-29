8e1e101583b6b2c3451d7609ec1f6f986eaf78ac
Alexander <sany199574@gmail.com>
2023-11-29 05:45:11 +0300

    Реализован репощиторий - CurrencyRepository
    Исправлены ошибки, в коде

--- Stock/Interfaces/ICurrency.cs
+++ Stock/Interfaces/ICurrency.cs
@@ -7,13 +7,14 @@ namespace Stock.Interfaces
     {
         PaginatedList<Currency> GetItems(string sortProperty, SortOrder order, string searchText, int pageIndex, int pageSize);
         Currency GetItem(Guid id);
-        Currency GetItem_NoDownload_FG(string code);
         bool Greate(Currency item);
         bool Edit(Currency item);
         bool Delete(Currency item);
 
         public bool IsItemNameExists(string name);
         public bool IsItemNameExists(string name, Guid id);
-        public bool IsCurrencyCombExists(string srcIsCurrencyId, Guid excCurrencyId);
+        public bool IsCurrencyCombExists(Guid srcIsCurrencyId, Guid excCurrencyId);
+
+        public string GetErrors();
     }
 }
---
+++ Stock/Repositories/CurrencyRepository.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Stock.Data;
+using Stock.Interfaces;
+using Stock.Models;
+
+namespace Stock.Repositories
+{
+    public class CurrencyRepository : ICurrency
+    {
+        private readonly InventoryContext _context;
+        private string _errors = string.Empty;
+
+        public CurrencyRepository(InventoryContext context)
+        {
+            _context = context;
+        }
+        public PaginatedList<Currency> GetItems(string sortProperty, SortOrder order, string searchText, int pageIndex, int pageSize)
+        {
+            List<Currency> items;
+
+            if (searchText != "" && searchText != null)
+            {
+                items = _context.Currencies
+                    .Where(x => x.Name.Contains(searchText) || x.Description.Contains(searchText))
+                    .ToList();
+            }
+            else
+            {
+                items = _context.Currencies.ToList();
+            }
+
+            items = DoSort(items, sortProperty, order);
+
+            PaginatedList<Currency> retUnits = new PaginatedList<Currency>(items, pageIndex, pageSize);
+
+            return retUnits;
+        }
+
+        public Currency GetItem(Guid id) => _context.Currencies.FirstOrDefault(x => x.Id == id);
+
+
+        public bool Greate(Currency item)
+        {
+            try
+            {
+                _context.Currencies.Add(item);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                _errors = "Create failed - Sql Exception Occured, Error info: " + e.Message;
+                return false;
+            }
+           
+        }
+
+        public bool Edit(Currency item)
+        {
+            try
+            {
+                _context.Currencies.Attach(item);
+                _context.Entry(item).State = EntityState.Modified;
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                _errors = "Edit Failed - Sql Exception Occured, Error info: " + e.Message;
+                return false;
+            }
+        }
+
+        public bool Delete(Currency item)
+        {
+            try
+            {
+                _context.Currencies.Attach(item);
+                _context.Entry(item).State = EntityState.Deleted;
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                _errors = "Delete failed - Sql Exception Occured, Error info: " + e.Message;
+                return false;
+            }
+
+        }
+
+        public bool IsItemNameExists(string name)
+        {
+            int ct = _context.Currencies.Where(x => x.Name.ToLower() == name.ToLower()).Count();
+            if (ct > 0)
+                return true;
+            else
+                return false;
+        }
+        public bool IsItemNameExists(string name, Guid id)
+        {
+            int ct = _context.Currencies.Where(x => x.Name.ToLower() == name.ToLower() && x.Id != id).Count();
+            if (ct > 0)
+                return true;
+            else
+                return false;
+        }
+        public bool IsCurrencyCombExists(Guid srcIsCurrencyId, Guid excCurrencyId)
+        {
+            int ct = _context.Currencies.Where(x => x.Id == srcIsCurrencyId && x.ExchangeCurrencyId == excCurrencyId).Count();
+            if (ct > 0)
+                return true;
+            else
+                return false;
+        }
+
+        public string GetErrors()
+        {
+            return _errors;
+        }
+
+        private List<Currency> DoSort(List<Currency> items, string sortProperty, SortOrder order)
+        {
+            if (sortProperty.ToLower() == "name")
+            {
+                if (order == SortOrder.Ascending)
+                    items = items.OrderBy(x => x.Name).ToList();
+                else
+                    items = items.OrderByDescending(x => x.Name).ToList();
+            }
+            else if (sortProperty.ToLower() == "description")
+            {
+                if (order == SortOrder.Ascending)
+                    items = items.OrderBy(x => x.Description).ToList();
+                else
+                    items = items.OrderByDescending(x => x.Description).ToList();
+            }
+
+            return items;
+        }
+    }
+}
