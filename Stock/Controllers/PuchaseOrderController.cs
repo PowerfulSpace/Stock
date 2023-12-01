779cb872cc69753e1213a7ac76c18be696c14fc7
Alexander <sany199574@gmail.com>
2023-12-01 09:41:35 +0300

    Исправил метод Create в контроллере PuchaseOrderController  реализовал Get и Post методы для Create

--- Stock/Controllers/PuchaseOrderController.cs
+++ Stock/Controllers/PuchaseOrderController.cs
@@ -40,11 +40,51 @@ namespace Stock.Controllers
             return View(products);
         }
 
+
+
+
         [HttpGet]
         public IActionResult Create()
         {
             var poHeader = new PoHeader();
+
+            //PopulateViewBags();
+
             return View(poHeader);
         }
+
+        [HttpPost]
+        public IActionResult Create(PoHeader poHeader)
+        {
+            var bolret = false;
+            string errMessage = "";
+
+            try
+            {
+                bolret = _puchaseOrderRepo.Greate(poHeader);
+            }
+            catch (Exception ex)
+            {
+                errMessage = errMessage + " " + ex.Message;
+            }
+
+            if (bolret == false)
+            {
+                errMessage = errMessage + " " + _puchaseOrderRepo.GetErrors();
+                TempData["ErrorMessage"] = errMessage;
+                ModelState.AddModelError("", errMessage);
+
+                //PopulateViewBags();
+
+                return View(poHeader);
+            }
+            else
+            {
+                TempData["SuccessMessage"] = "PoHeader " + poHeader.PoNumber + " Created Successfully";
+
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
     }
 }
